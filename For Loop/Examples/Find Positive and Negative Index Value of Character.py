4a6f61b173bd8df02f32d90d875ebcad3711b5c4
Riya Halbhavi <riyahal.dev@gmail.com>
2024-12-31 18:46:14 +0530

    Add files via upload

---
+++ For Loop/Examples/Find Positive and Negative Index Value of Character.py
@@ -0,0 +1,16 @@
+### Find the positive and negative index value of 'U' in the following string: str = "PES University"###
+
+str2 = "PES University"
+
+index = str2.index("U")
+print(index)
+# Output: 4
+
+for i in range(len(str2)):
+           if str2[i] == "U":
+               positive_index = index
+               negative_index = index - len(str2)
+               print ("Negative index: ", negative_index)
+
+# Output: Negative index: -10
+
