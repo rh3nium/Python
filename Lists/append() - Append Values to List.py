aeb40ae406b16967d7e8f221f8064d6af8a935a9
Riya H <riyahal.dev@gmail.com>
2025-06-13 13:25:33 +0530

    Update append() - Append Values to List.py

--- Lists/append() - Append Values to List.py
+++ Lists/append() - Append Values to List.py
@@ -4,13 +4,12 @@ List1 = ["carrot", "potato", "lettuce", "beans", "corn"]
 
 # Add/append a value to the list
 List1.append("capsicum")
-
+# Print updated list
 print (List1)
-
 
 List2 = ["a", "b", "c", "d", "e"]
 
 # Add/append a value to the list
 List2.append("f")
-
+# Print updated list
 print (List2)
