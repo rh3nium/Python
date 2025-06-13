7b4ae2c35a10bc474ef58a51f5a761c0bc92dc37
Riya H <riyahal.dev@gmail.com>
2025-06-13 13:30:45 +0530

    Update update() - Add:Append Values to Dictionary.py

--- Dictionaries/update() - Add:Append Values to Dictionary.py
+++ Dictionaries/update() - Add:Append Values to Dictionary.py
@@ -1,10 +1,8 @@
-### Sets - Add/Append Values to a Set ###
-
+### Append Values to dictionary ###
 
 dict1 = {1: 'Python', 2: 'Java', 3: 'C++'}
 
 # Add/append values to the dictionary using update() function
-
 dict1.update({"4": "Ruby"})
 
 # print updated dictionary
