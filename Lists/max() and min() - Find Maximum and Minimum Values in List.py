6151cd0ec7b34cf24bc8bfab08f3fc9964977324
Riya H <riyahal.dev@gmail.com>
2025-06-13 13:12:16 +0530

    Update max() and min() - Find Maximum and Minimum Values in List.py

--- Lists/max() and min() - Find Maximum and Minimum Values in List.py
+++ Lists/max() and min() - Find Maximum and Minimum Values in List.py
@@ -1,11 +1,11 @@
-### Lists - Sorting Values in a List ######
+### max(), min() - Find maximum and minimum values in list ######
 
 list1 = [12,43,21,1]
 
-# Find max/largest value in the list
+# max() - Find max/largest value in the list
 max(list1)
 print("Max value in the list: ", max(list1))
 
-# Find min/smallest value in the list
+# min() - Find min/smallest value in the list
 min(list1)
 print("Min value in the list: ", min(list1))
