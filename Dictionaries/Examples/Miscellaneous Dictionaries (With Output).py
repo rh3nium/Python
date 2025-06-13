86bf8f2448b9a330e6df3aaeb7f84442250e6169
Riya H <riyahal.dev@gmail.com>
2025-06-13 13:31:15 +0530

    Update Miscellaneous Dictionaries (With Output).py

--- Dictionaries/Examples/Miscellaneous Dictionaries (With Output).py
+++ Dictionaries/Examples/Miscellaneous Dictionaries (With Output).py
@@ -1,6 +1,5 @@
 ### Miscellaneous Dictionaries (With Output) ###
 
-
 d = {1:'amit', 2:'banu', 3:'chandan', 4:'charan', 5:'divya'}
 
 print (len(d)) # Output: 5
@@ -9,5 +8,3 @@ d.pop(5)
 print(d) # Output: {1: 'amit', 2: 'banu', 3: 'chandan', 4: 'charan'}
 d.clear()
 print(d) # Output: {}
-
-
