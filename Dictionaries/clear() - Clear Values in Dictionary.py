0a877adccb455cd5f60433d7687b7feed91fb33f
Riya H <riyahal.dev@gmail.com>
2025-06-13 13:29:25 +0530

    Update clear() - Clear Values in Dictionary.py

--- Dictionaries/clear() - Clear Values in Dictionary.py
+++ Dictionaries/clear() - Clear Values in Dictionary.py
@@ -1,10 +1,9 @@
-### Dictionaries - clear() - Clear Values ###
-
+### clear() - Clear Values in dictionary ###
 
 dict1 = {1: 'Python', 2: 'Java', 3: 'C++'}
 
-# Clear all the values in the dictionary
+# Clear all values in the dictionary
 dict1.clear()
 
-# print the updated dictionary to verify
+# print updated dictionary
 print (dict1)
