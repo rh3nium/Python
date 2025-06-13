6c989b050c2ff0218cce620a828e86186aeab4bf
Riya H <riyahal.dev@gmail.com>
2025-06-13 13:30:21 +0530

    Update pop() - Delete Values in Dictionary.py

--- Dictionaries/pop() - Delete Values in Dictionary.py
+++ Dictionaries/pop() - Delete Values in Dictionary.py
@@ -1,5 +1,4 @@
-### Dictionaries - Delete Values using pop() function ######
-
+### pop() - Delete Values in dictionary ###
 
 dict1 = {1: 'Python', 2: 'Java', 3: 'C++'}
 print (dict1)
@@ -7,5 +6,5 @@ print (dict1)
 # delete element in dictionary using pop() function, specifying index
 dict1.pop(3)
 
-# Print the updated dictionary
+# Print updated dictionary
 print (dict1)
